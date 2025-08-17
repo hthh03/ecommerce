fec7e78478134be81a6ccc855b6023ab751867c7
hthh03 <haoh5003@gmail.com>
2025-08-17 23:17:26 +0700

    backend api basic userController and add product

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,23 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    try {
+        console.log("MONGODB_URI:", process.env.MONGODB_URI); 
+        
+        mongoose.connection.on('connected', () => {
+            console.log("DB Connected");
+        });
 
-    mongoose.connection.on('connected',() => {
-        console.log("DB Connected")
-    })
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
+        mongoose.connection.on('error', (err) => {
+            console.log("DB Connection Error:", err);
+        });
+        
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
+        
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
 }
 
 export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 // App config
 const app = express()
@@ -15,8 +17,11 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
- app.get('/',(req,res) => {
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+
+app.get('/',(req,res) => {
     res.send("Api Working")
  })
 
- app.listen(port, ()=> console.log('Server started on PORT: ' + port))
+app.listen(port, ()=> console.log('Server started on PORT: ' + port))
