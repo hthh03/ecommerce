158fb2eb00709a7c4c7200a1d62c31238a4eee1f
hthh03 <haoh5003@gmail.com>
2025-08-19 20:58:34 +0700

    api user admin and setup BE for products

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,14 +77,25 @@ const registerUser = async (req,res) => {
 
     } catch (error) {
         console.log(error);
-         res.status(500).json({success: false, message: "Server error"});
+        res.status(500).json({success: false, message: "Server error"});
     }
 }
 
-
 //route for admin login 
 const adminLogin = async (req,res) => {
+    try {
+        const {email,password} = req.body
 
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password,process.env.JWT_SECRET);
+            res.json({success:true,token})
+        } else {
+            res.json({success:false,message:"Invalid credentials"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Server error"});
+    }
 }
 
 export {loginUser, registerUser, adminLogin}
